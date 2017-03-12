121f83f124c606d865722a5aa16e098739c6886f
Zura Guerra <guerrazuramx@gmail.com>
2017-03-12 01:52:57 -0600

    Save photos

--- web/models/artist.ex
+++ web/models/artist.ex
@@ -1,10 +1,11 @@
 defmodule Catrina.Artist do
   use Catrina.Web, :model
+  use Arc.Ecto.Schema
 
   schema "artists" do
     field :name, :string
     field :description, :string
-    field :photo_url, :string
+    field :photo_url, Catrina.ArtistPhoto.Type
     belongs_to :artist_categories, Catrina.ArtistCategories
     has_many :artist_urls, Catrina.ArtistUrls
 
@@ -17,6 +18,7 @@ defmodule Catrina.Artist do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :description, :photo_url, :artist_categories_id])
+    |> cast_attachments(params, [:photo_url])
     |> validate_required([:name, :description, :photo_url, :artist_categories_id])
   end
 end
--- web/models/frame_photos.ex
+++ web/models/frame_photos.ex
@@ -1,8 +1,9 @@
 defmodule Catrina.FramePhotos do
   use Catrina.Web, :model
+  use Arc.Ecto.Schema
 
   schema "frame_photos" do
-    field :photo_url, :string
+    field :photo_url, Catrina.FramePhoto.Type
     belongs_to :frame, Catrina.Frame
 
     timestamps()
@@ -14,6 +15,7 @@ defmodule Catrina.FramePhotos do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:photo_url, :frame_id])
+    |> cast_attachments(params, [:photo_url])
     |> validate_required([:photo_url, :frame_id])
   end
 end
